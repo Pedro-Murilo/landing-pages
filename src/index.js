cbe9caa36261d87533635a7cb704185ed5f5a55b
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-11 17:27:14 -0300

    config: ThemeProvider and GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/App';
+import GlobalStyles from './styles/GlobalStyles';
+import { theme } from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    bgDark: '#252525',
+  },
+  spacing: {
+    pY: '2.5rem',
+  },
+};
