142dec644890e1f3181b09e02e63c5baef2b6510
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-15 09:15:59 -0300

    test: tested all components and created snapshot test

--- src/components/NavLink/NavLink.test.js
+++ src/components/NavLink/NavLink.test.js
@@ -2,10 +2,33 @@ import { renderTheme } from '../../styles/RenderTheme';
 import { screen } from '@testing-library/react';
 
 import { NavLink } from '.';
+import mock from './mock';
+import { theme } from '../../styles/theme';
 
 describe('<NavLink />', () => {
-  it('should render', () => {
-    renderTheme(<NavLink>children</NavLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render links', () => {
+    renderTheme(<NavLink links={mock} />);
+    expect(screen.getAllByRole('link')).toHaveLength(mock.length);
+  });
+
+  it('should not render links', () => {
+    renderTheme(<NavLink />);
+    expect(screen.queryAllByText(/links/i)).toHaveLength(0);
+  });
+
+  it('should render links', () => {
+    renderTheme(<NavLink links={mock} />);
+    expect(screen.getByText(/link 5/i).parentElement).toHaveStyleRule(
+      'flex-flow',
+      'column wrap',
+      {
+        media: theme.media.lteMedium,
+      },
+    );
+  });
+
+  it('should match snapshot', () => {
+    renderTheme(<NavLink links={mock} />);
+    expect(screen.getAllByRole('link')).toMatchSnapshot();
   });
 });
--- src/components/NavLink/index.js
+++ src/components/NavLink/index.js
@@ -20,5 +20,5 @@ NavLink.propTypes = {
       link: P.string.isRequired,
       newTab: P.bool,
     }),
-  ).isRequired,
+  ),
 };
