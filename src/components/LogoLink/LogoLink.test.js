4b4f91fdfa4f73721353dbbcc60e50efc2e4955d
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-15 16:06:04 -0300

    test: tested LogoLink and match snapshot

--- src/components/LogoLink/LogoLink.test.js
+++ src/components/LogoLink/LogoLink.test.js
@@ -4,8 +4,29 @@ import { screen } from '@testing-library/react';
 import { LogoLink } from '.';
 
 describe('<LogoLink />', () => {
-  it('should render', () => {
-    renderTheme(<LogoLink>children</LogoLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render text logo', () => {
+    renderTheme(<LogoLink link="#target" text="Logo" />);
+
+    expect(screen.getByRole('link', { name: 'Logo' })).toHaveAttribute(
+      'href',
+      '#target',
+    );
+  });
+
+  it('should render image logo', () => {
+    renderTheme(<LogoLink link="#target" text="Logo" srcImg="image.jpg" />);
+
+    expect(screen.getByRole('img', { name: 'Logo' })).toHaveAttribute(
+      'src',
+      'image.jpg',
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(
+      <LogoLink link="#target" text="Logo" srcImg="image.jpg" />,
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
