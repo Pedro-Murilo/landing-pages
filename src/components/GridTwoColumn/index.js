38a0720f67894049de58fd64430c1240ae414cbb
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-16 14:55:04 -0300

    component: created GridTwoColumn component

---
+++ src/components/GridTwoColumn/index.js
@@ -0,0 +1,29 @@
+import P from 'prop-types';
+import { Container, Image, ImageContainer, TextContainer } from './styles';
+
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridTwoColumn = ({ title, text, srcImg, background }) => {
+  return (
+    <SectionBackground background={background}>
+      <Container background={background}>
+        <TextContainer>
+          <Heading uppercase>{title}</Heading>
+          <TextComponent>{text}</TextComponent>
+        </TextContainer>
+      </Container>
+      <ImageContainer>
+        <Image src={srcImg} alt={title} />
+      </ImageContainer>
+    </SectionBackground>
+  );
+};
+
+GridTwoColumn.propTypes = {
+  title: P.string.isRequired,
+  text: P.string.isRequired,
+  srcImg: P.string.isRequired,
+  background: P.bool,
+};
