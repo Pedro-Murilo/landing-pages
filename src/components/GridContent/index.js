15197178e1efe8534cc465d32bb4dbd26748b14d
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-17 10:46:04 -0300

    component: created GridContent component

---
+++ src/components/GridContent/index.js
@@ -0,0 +1,26 @@
+import P from 'prop-types';
+import { Container, Html } from './styles';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridContent = ({ title, html, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Container>
+        <Heading uppercase colorDark={!background}>
+          {title}
+        </Heading>
+        <Html>
+          <TextComponent>{html}</TextComponent>
+        </Html>
+      </Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: P.string.isRequired,
+  html: P.string.isRequired,
+  background: P.bool,
+};
