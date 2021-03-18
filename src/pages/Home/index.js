9c2470f45d197ddf9e976fd3c58bd6d0c3e997b3
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-18 11:25:20 -0300

    feat: created loading on fetch data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,29 +3,36 @@ import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
 
 import { mapData } from '../../api/map-data';
+import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
 
 function Home() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const load = async () => {
-      const data = await fetch(
-        'http://localhost:1337/pages/?slug=landing-page',
-      );
-      const json = await data.json();
-      const pageData = mapData(json);
-      setData(pageData[0]);
+      try {
+        const data = await fetch(
+          'http://localhost:1337/pages/?slug=landing-page',
+        );
+        const json = await data.json();
+        const pageData = mapData(json);
+
+        setData(pageData[0]);
+      } catch (e) {
+        setData(undefined);
+      }
     };
 
     load();
   }, []);
 
   if (data === undefined) {
-    return <h1>Página não encontrada</h1>;
+    return <PageNotFound />;
   }
 
   if (data && !data.slug) {
-    return <h1>Carregando...</h1>;
+    return <Loading />;
   }
 
   return <Base {...mockBase} />;
---
+++ src/pages/Loading/index.js
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export const Loading = () => {
+  return (
+    <Container>
+      <h1></h1>
+    </Container>
+  );
+};
